3188f457f804d0c4df13de22eeb66059e6028383
Oliver Smith <oliver@kfs.org>
2017-07-05 22:21:59 -0700

    Refactored remove.sh *UNTESTED*

--- remove.sh
+++ remove.sh
@@ -31,104 +31,85 @@
 # Additions by btidey, miraaz, gigpi
 # Split up and refactored by Bob Tidey 
 
-#Debug enable next 3 lines
-exec 5> remove.txt
-BASH_XTRACEFD="5"
-set -x
-
-
+# Bootstrap.
 cd $(dirname $(readlink -f $0))
+source lib/common.shfrag
+fn_enable_debug_log remove.txt
+fn_require_dialog
+fn_load_config false  # Load or generate the config file.
 
-# Terminal colors
-color_red="tput setaf 1"
-color_green="tput setaf 2"
-color_reset="tput sgr0"
-
-source ./config.txt
-
-fn_stop ()
-{ # This is function stop
-        sudo killall raspimjpeg 2>/dev/null
-        sudo killall php 2>/dev/null
-        sudo killall motion 2>/dev/null
-        sudo service apache2 stop >dev/null 2>&1
-        sudo service nginx stop >dev/null 2>&1
-        sudo service lighttpd stop >dev/null 2>&1
-        dialog --title 'Stop message' --infobox 'Stopped.' 4 16 ; sleep 2
-}
-
-fn_autostart_disable ()
+function fn_apache_default ()
 {
-  tmpfile=$(mktemp)
-  sudo sed '/#START/,/#END/d' /etc/rc.local > "$tmpfile" && sudo mv "$tmpfile" /etc/rc.local
-  # Remove to growing plank lines.
-  sudo awk '!NF {if (++n <= 1) print; next}; {n=0;print}' /etc/rc.local > "$tmpfile" && sudo mv "$tmpfile" /etc/rc.local
-			  
-  # Finally we set owners and permissions all files what we changed.
-  sudo chown root:root /etc/rc.local
-  sudo chmod 755 /etc/rc.local
-  sudo chmod 664 ./config.txt
+    if [ -e /etc/apache2/sites-available/000-default.conf ]; then
+        adefault="/etc/apache2/sites-available/000-default.conf"
+        subdir="\/html"
+    else
+        adefault="/etc/apache2/sites-available/default"
+        subdir=""
+    fi
+    tmpfile=$(tempfile)
+    webport="80"
+    user=""
+    passwd=""
+    sudo awk '/NameVirtualHost \*:/{c+=1}{if(c==1){sub("NameVirtualHost \*:.*","NameVirtualHost *:'$webport'",$0)};print}' /etc/apache2/ports.conf > "$tmpfile" && sudo mv "$tmpfile" /etc/apache2/ports.conf
+    sudo awk '/Listen/{c+=1}{if(c==1){sub("Listen.*","Listen '$webport'",$0)};print}' /etc/apache2/ports.conf > "$tmpfile" && sudo mv "$tmpfile" /etc/apache2/ports.conf
+    sudo awk '/<VirtualHost \*:/{c+=1}{if(c==1){sub("<VirtualHost \*:.*","<VirtualHost *:'$webport'>",$0)};print}' $adefault > "$tmpfile" && sudo mv "$tmpfile" $adefault
+    sudo sed -i "s/DocumentRoot\ \/var\/www\/.*/DocumentRoot\ \/var\/www$subdir/g" $adefault
+    sudo awk '/AllowOverride/{c+=1}{if(c==2){sub("AllowOverride.*","AllowOverride None",$0)};print}' /etc/apache2/sites-available/default > "$tmpfile" && sudo mv "$tmpfile" $adefault
+    sudo service apache2 restart
 }
 
+##############################################################################
+### Main
+##############################################################################
 
-fn_apache_default ()
-{
-if [ -e /etc/apache2/sites-available/000-default.conf ]; then
-   adefault="/etc/apache2/sites-available/000-default.conf"
-   subdir="\/html"
-else
-   adefault="/etc/apache2/sites-available/default"
-   subdir=""
-fi
-tmpfile=$(mktemp)
-webport="80"
-user=""
-passwd=""
-sudo awk '/NameVirtualHost \*:/{c+=1}{if(c==1){sub("NameVirtualHost \*:.*","NameVirtualHost *:'$webport'",$0)};print}' /etc/apache2/ports.conf > "$tmpfile" && sudo mv "$tmpfile" /etc/apache2/ports.conf
-sudo awk '/Listen/{c+=1}{if(c==1){sub("Listen.*","Listen '$webport'",$0)};print}' /etc/apache2/ports.conf > "$tmpfile" && sudo mv "$tmpfile" /etc/apache2/ports.conf
-sudo awk '/<VirtualHost \*:/{c+=1}{if(c==1){sub("<VirtualHost \*:.*","<VirtualHost *:'$webport'>",$0)};print}' $adefault > "$tmpfile" && sudo mv "$tmpfile" $adefault
-sudo sed -i "s/DocumentRoot\ \/var\/www\/.*/DocumentRoot\ \/var\/www$subdir/g" $adefault
-sudo awk '/AllowOverride/{c+=1}{if(c==2){sub("AllowOverride.*","AllowOverride None",$0)};print}' /etc/apache2/sites-available/default > "$tmpfile" && sudo mv "$tmpfile" $adefault
-sudo service apache2 restart
-}
-
-
+fn_info "Stopping any running instances."
 fn_stop
-     
-dialog --title "Uninstall packages!" --backtitle "$backtitle" --yesno "Do You want uninstall webserver and php packages also?" 6 35
+
+fn_ask "Uninstall Packages?" "Uninstall webserver and php packages too?"
 response=$?
 case $response in
-   0) package=('apache2' 'php5' 'libapache2-mod-php5' 'php5-cli' 'zip' 'nginx' 'lighttpd ''apache2-utils' 'php5-fpm' 'php5-common' 'php-apc' 'gpac motion' 'libav-tools');; 
-   1) package=('zip' 'gpac motion' 'libav-tools');; 
-   255) dialog --title 'Uninstall message' --infobox 'Webserver and php packages not uninstalled.' 4 33 ; sleep 2;;
+    0) package=('apache2' 'php5' 'libapache2-mod-php5' 'php5-cli' 'zip' 'nginx' 'lighttpd ''apache2-utils' 'php5-fpm' 'php5-common' 'php-apc' 'gpac motion' 'libav-tools');; 
+    1) package=('zip' 'gpac motion' 'libav-tools');; 
+    255) fn_feedback 2 'Packages' 'Webserver and php packages not uninstalled.'
 esac
+
+# whittle the uninstall list down to packages actually installed.
+uninstalls=""
 for i in "${package[@]}"
-   do
-      if [ $(dpkg-query -W -f='${Status}' "$i" 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-      sudo apt-get remove -y "$i"
-      fi
-   done
-sudo apt-get autoremove -y	  
+do
+    if is_installed "${i}"; then uninstalls+=" $i"; fi
+done
+if [ -n "${uninstalls}" ]; then
+    # only remove packages that were uninstalled.
+    fn_feedback 2 "Uninstalling packages" "${uninstalls}"
+    sudo apt-get remove --purge "${uninstalls}"
+fi
+
+#sudo apt-get autoremove -y	  # this presumes other packages aren't out of date
 
 if [ ! -d ~/media ]; then
   mkdir ~/media
 fi
 if [ ! "$rpicamdir" == "" ]; then
-   sudo mv  /var/www/$rpicamdir/media ~/media
-   sudo rm -r /var/www/$rpicamdir
+    sudo mv /var/www/$rpicamdir/media ~/media
+    sudo rm -r /var/www/$rpicamdir
 else
-   sudo mv /var/www/media ~/media
-   sudo rm /var/www/*
+    sudo mv /var/www/media ~/media
+    sudo rm /var/www/*
 fi
 sudo rm /etc/sudoers.d/RPI_Cam_Web_Interface
 sudo rm /usr/bin/raspimjpeg
 sudo rm /etc/raspimjpeg
+
+fn_info "Removing autostart configuration."
 fn_autostart_disable
+
+fn_set_configfile_perms
 
 sudo mv etc/nginx/sites-available/*default* /etc/nginx/sites-available >/dev/null 2>&1
 sudo mv etc/apache2/sites-available/*default* /etc/apache2/sites-available >/dev/null 2>&1
      
-if [ $(dpkg-query -W -f='${Status}' "apache2" 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-   fn_apache_default
+if is_installed "apache2"; then
+    fn_apache_default
 fi
-
