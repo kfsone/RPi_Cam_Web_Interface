459a2d80e3c83ba36815a953547f0db16398ef12
Oliver Smith <oliver@kfs.org>
2017-07-05 21:58:39 -0700

    Refactored update.sh to use common.shfrag

--- update.sh
+++ update.sh
@@ -31,52 +31,35 @@
 # Additions by btidey, miraaz, gigpi
 # Split up and refactored by Bob Tidey 
 
-#Debug enable next 3 lines
-exec 5> update.txt
-BASH_XTRACEFD="5"
-set -x
 
+# Bootstrap
 cd $(dirname $(readlink -f $0))
-
-# Terminal colors
-color_red="tput setaf 1"
-color_green="tput setaf 2"
-color_reset="tput sgr0"
+source lib/common.shfrag
+fn_enable_debug_log update.txt
+fn_require_dialog
 
 
-fn_abort()
-{
-    $color_red; echo >&2 '
-***************
-*** ABORTED ***
-***************
-'
-    echo "An error occurred. Exiting..." >&2; $color_reset
-    exit 1
-}
+trap 'fn_abort' 0
+set -e
 
-   trap 'fn_abort' 0
-   set -e
-   remote=$(
-     git ls-remote -h origin master |
-     awk '{print $1}'
-   )
-   local=$(git rev-parse HEAD)
-   printf "Local : %s\nRemote: %s\n" $local $remote
-   if [[ $local == $remote ]]; then
-      dialog --title 'Update message' --infobox 'Commits match. Nothing update.' 4 35 ; sleep 2
-   else
-      dialog --title 'Update message' --infobox "Commits don't match. We update." 4 35 ; sleep 2
-      git fetch origin master
-      git reset --hard origin/master
-      chmod u+x *.sh
-   fi
-   trap : 0
-   dialog --title 'Update message' --infobox 'Update finished.' 4 20 ; sleep 2
-   
-   # We call updated install script passing through any quiet parameter
-   if [ $# -eq 0 ]; then 
-      ./install.sh
-   else
-      ./install.sh $1
-   fi
+fn_info "Getting commit hashes..."
+remote=$(
+ git ls-remote -h origin master |
+ awk '{print $1}'
+)
+local=$(git rev-parse HEAD)
+
+printf "Local : %s\nRemote: %s\n" $local $remote
+
+if [[ $local == $remote ]]; then
+    fn_feedback 2 "Update status" "Commits match: Nothing to update."
+else
+    fn_feedback 2 "Update status" "Commits don't match, performing update."
+    git fetch origin master
+    git reset --hard origin/master
+    fn_feedback 2 "Update status" "Update finished."
+fi
+trap : 0
+
+# We call updated install script passing through any quiet parameter
+./install.sh $*
