40470ef1a36a026e0327da89032a0345658859ca
Oliver Smith <oliver@kfs.org>
2017-07-05 22:21:43 -0700

    Refactored cam.sh with common.shfrag

--- cam.sh
+++ cam.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
 
+# Bootstrap.
 cd $(dirname $(readlink -f $0))
-
-if [ $(dpkg-query -W -f='${Status}' "dialog" 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-  sudo apt-get install -y dialog
-fi
-
-# Terminal colors
-color_red="tput setaf 1"
-color_green="tput setaf 2"
-color_reset="tput sgr0"
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-NORMAL=$(tput sgr0)
+source lib/common.shfrag
+source ./lib/webservers.shfrag
+fn_enable_debug_log cam.txt
+fn_require_dialog
+fn_load_config true  # Load or generate the config file.
 
 ASK_TO_REBOOT=0
-RASPICONFIG=/boot/config.txt
 MAINSCRIPT="./RPi_Cam_Web_Interface_Installer.sh"
 
-# Version stuff moved out functions as we need it more when one time.
-versionfile="./www/config.php"
-version=$(cat $versionfile | grep "'APP_VERSION'" | cut -d "'" -f4)
-backtitle="Copyright (c) 2014, Silvan Melchior. RPi Cam $version"
-
-get_config_var() {
-  lua - "$1" "$2" <<EOF
+function get_config_var()
+{
+    lua - "$1" "$2" <<EOF
 local key=assert(arg[1])
 local fn=assert(arg[2])
 local file=assert(io.open(fn))
@@ -38,13 +27,14 @@ for line in file:lines() do
   end
 end
 if not found then
-   print(0)
+  print(0)
 end
 EOF
 }
 
-set_config_var() {
-  lua - "$1" "$2" "$3" <<EOF > "$3.bak"
+function set_config_var()
+{
+    lua - "$1" "$2" "$3" <<EOF > "$3.bak"
 local key=assert(arg[1])
 local value=assert(arg[2])
 local fn=assert(arg[3])
@@ -62,62 +52,80 @@ if not made_change then
   print(key.."="..value)
 end
 EOF
-mv "$3.bak" "$3"
+    mv "$3.bak" "$3"
 }
 
-do_finish() {
-  if [ $ASK_TO_REBOOT -eq 1 ]; then
-    dialog --yesno "Would you like to reboot now?" 5 35
-    if [ $? -eq 0 ]; then # yes
-      sync
-      reboot
-	elif [ $? -eq 1 ]; then # no
-	  exec sudo $MAINSCRIPT
-    fi
-  fi
-  exit 0
-}
 
-do_camera ()
+# Helper: exit cleanly or ask the user to reboot.
+# If the user is asked to reboot and declines, run the MAINSCRIPT.
+#
+function do_finish()
 {
-  if [ ! -e /boot/start_x.elf ]; then
-    dialog --msgbox "Your firmware appears to be out of date (no start_x.elf). Please update" 20 60
-	exec sudo $MAINSCRIPT
-  fi
-  dialog --title "Raspberry Pi camera message" \
-  --backtitle "$backtitle"                     \
-  --extra-button --extra-label Disable         \
-  --ok-label Enable                            \
-  --yesno "Enable support for Raspberry Pi camera?" 5 48
-  response=$?
-  case $response in
-    0) #echo "[Enable] key pressed."
-    set_config_var start_x 1 $RASPICONFIG
-    CUR_GPU_MEM=$(get_config_var gpu_mem $RASPICONFIG)
-    if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
-      set_config_var gpu_mem 128 $RASPICONFIG
+    if [ $ASK_TO_REBOOT -ne 1 ]; then
+        exit 0
     fi
-    sed $RASPICONFIG -i -e "s/^startx/#startx/"
-    sed $RASPICONFIG -i -e "s/^fixup_file/#fixup_file/"
-	ASK_TO_REBOOT=1
-	do_finish
-    ;;
-    1) #echo "[Cansel] key pressed."
-    exec sudo $MAINSCRIPT
-    ;;
-    3) #echo "[Disable] key pressed."
-    set_config_var start_x 0 $RASPICONFIG
-    sed $RASPICONFIG -i -e "s/^startx/#startx/"
-    sed $RASPICONFIG -i -e "s/^start_file/#start_file/"
-    sed $RASPICONFIG -i -e "s/^fixup_file/#fixup_file/"
-	ASK_TO_REBOOT=1
-	do_finish
-    ;;
-    255) echo "[ESC] key pressed."
-    exec sudo $MAINSCRIPT
-    ;;
-  esac
+    fn_ask "Reboot?" "Would you like to reboot now?"
+    if [ $? -eq 0 ]; then # yes
+        sync && sync
+        reboot
+    elif [ $? -eq 1 ]; then # no
+        exec sudo $MAINSCRIPT
+    fi
 }
+
+
+function do_camera ()
+{
+    if [ ! -e /boot/start_x.elf ]; then
+        fn_feedback 5 \
+            "Missing start_x.elf" \
+            "Your firmware appears to be out of date, please update it!"
+	    exec sudo $MAINSCRIPT
+    fi
+
+    fn_ask "Raspberry Pi camera message" \
+        "Enable support for Raspberry Pi camera?" \
+        --extra-button --extra-label Disable --ok-label Enable
+    response=$?
+    case $response in
+        0)
+            fn_info "[Enable] selected."
+            set_config_var start_x 1 $RASPICONFIG
+            CUR_GPU_MEM=$(get_config_var gpu_mem $RASPICONFIG)
+            if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
+                set_config_var gpu_mem 128 $RASPICONFIG
+            fi
+            sed $RASPICONFIG -i -e "s/^startx/#startx/"
+            sed $RASPICONFIG -i -e "s/^fixup_file/#fixup_file/"
+            ASK_TO_REBOOT=1
+            do_finish
+        ;;
+        1)
+            fn_info "[Cancel] selected."
+            exec sudo $MAINSCRIPT
+        ;;
+        3)
+            fn_info "[Disable] selected."
+            set_config_var start_x 0 $RASPICONFIG
+            sed $RASPICONFIG -i -e "s/^startx/#startx/"
+            sed $RASPICONFIG -i -e "s/^start_file/#start_file/"
+            sed $RASPICONFIG -i -e "s/^fixup_file/#fixup_file/"
+            ASK_TO_REBOOT=1
+            do_finish
+        ;;
+        255)
+            fn_info "[Esc] pressed."
+            exec sudo $MAINSCRIPT
+        ;;
+    esac
+}
+
+
+##############################################################################
+### Main
+##############################################################################
+
 
 do_camera
-exit
+
+exit 1  # should never be reached
