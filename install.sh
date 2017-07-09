f99393149084a2ace8702cae0290cc2f0ea40ab2
Oliver Smith <oliver@kfs.org>
2017-07-09 15:32:35 -0700

    Further refactor of install.sh - not tested.

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ fn_load_config true  # Load or generate the config file.
 
 rpicamdirold="${rpicamdir}"
 if [ -n "${rpicamdirold}" ]; then
-  rpicamdirold=/$rpicamdirold
+  rpicamdirold="/${rpicamdirold}"
 fi
 
 # Allow for a quiet install
@@ -86,39 +86,44 @@ if [ $# -eq 0 ] || [ "$1" != "q" ]; then
     source "${configfile}"
 fi
 
-if [ -n "${rpicamdir}" ]; then
-    rpicamdirEsc="\\/$rpicamdir"
-    rpicamdir=/$rpicamdir
-else
-    rpicamdirEsc=""
-fi
 
+function fn_motion_cfg()
+{
+    local param="${1}"; shift
+    local setting="${1}"; shift
+
+    # Using '#' instead of '/' as separators.
+    # Lines that are commented out get swapped in
+    sudo sed -i "s#^\(; *\)${param}.*#${param} ${setting}#g" "${motionconf}"
+}
+
+function fn_motion_unset()
+{
+    local param="${1}"; shift
+    sudo sed -i "s#^ *${param}.*#; ${param}#g" "${motionconf}"
+} 
 
 # Helper function to configure motion detection.
 function fn_motion ()
 {
-    sudo sed -i "s/^; netcam_url.*/netcam_url/g" "${motionconf}"        
-    sudo sed -i "s/^netcam_url.*/netcam_url http:\/\/localhost:$webport$rpicamdirEsc\/cam_pic.php/g" "${motionconf}"        
+    fn_motion_cfg netcam_url "http://localhost:$webport${rpicamdir}/cam_pic.php"
     if [ "$user" == "" ]; then
-       sudo sed -i "s/^netcam_userpass.*/; netcam_userpass value/g" "${motionconf}"     
+        fn_motion_unset netcam_userpass
     else
-       sudo sed -i "s/^; netcam_userpass.*/netcam_userpass/g" "${motionconf}"       
-       sudo sed -i "s/^netcam_userpass.*/netcam_userpass $user:$webpasswd/g" "${motionconf}"        
+        fn_motion_cfg netcam_userpass "${user}:${webpasswd}"
     fi
-    sudo sed -i "s/^; on_event_start.*/on_event_start/g" "${motionconf}"        
-    sudo sed -i "s/^on_event_start.*/on_event_start echo -n \'1\' >\/var\/www$rpicamdirEsc\/FIFO1/g" "${motionconf}"        
-    sudo sed -i "s/^; on_event_end.*/on_event_end/g" "${motionconf}"        
-    sudo sed -i "s/^on_event_end.*/on_event_end echo -n \'0\' >\/var\/www$rpicamdirEsc\/FIFO1/g" "${motionconf}"        
-    sudo sed -i "s/control_port.*/control_port 6642/g" "${motionconf}"      
-    sudo sed -i "s/control_html_output.*/control_html_output off/g" "${motionconf}"     
-    sudo sed -i "s/^output_pictures.*/output_pictures off/g" "${motionconf}"        
-    sudo sed -i "s/^ffmpeg_output_movies on/ffmpeg_output_movies off/g" "${motionconf}"     
-    sudo sed -i "s/^ffmpeg_cap_new on/ffmpeg_cap_new off/g" "${motionconf}"     
-    sudo sed -i "s/^stream_port.*/stream_port 0/g" "${motionconf}"      
-    sudo sed -i "s/^webcam_port.*/webcam_port 0/g" "${motionconf}"      
-    sudo sed -i "s/^process_id_file/; process_id_file/g" "${motionconf}"
-    sudo sed -i "s/^videodevice/; videodevice/g" "${motionconf}"
-    sudo sed -i "s/^event_gap 60/event_gap 3/g" "${motionconf}"
+    fn_motion_cfg on_event_start "echo -n '1' >${_camdir}/FIFO1"
+    fn_motion_cfg on_event_end   "echo -n '0' >${_camdir}/FIFO1"
+    fn_motion_cfg control_port 6642
+    fn_motion_cfg control_html_output off
+    fn_motion_cfg output_pictures  off
+    fn_motion_cfg ffmpeg_output_movies off
+    fn_motion_cfg ffmpeg_cap_new off
+    fn_motion_cfg stream_port 0
+    fn_motion_cfg webcam_port 0
+    fn_motion_unset process_id_file
+    fn_motion_unset videodevice
+    sudo sed -i "s#^event_gap *60#event_gap 3#g" "${motionconf}"
     sudo chown motion:www-data -- "${motionconf}"
     sudo chmod ug=rw,o=r -- "${motionconf}"
 }
@@ -139,14 +144,14 @@ function fn_autostart ()
         tempfile=$(tempfile)
         cat <<EOF >"${tempfile}"
 ${RC_START}
-mkdir -p /dev/shm/mjpeg
-chown www-data:www-data /dev/shm/mjpeg
-chmod 777 /dev/shm/mjpeg
+mkdir -p ${MJPEG_DEV}
+chown www-data:www-data ${MJPEG_DEV}
+chmod 777 ${MJPEG_DEV}
 sleep 4;su -c 'raspimjpeg > /dev/null 2>&1 &' www-data
 if [ -e /etc/debian_version ]; then
-    sleep 4;su -c 'php /var/www$rpicamdir/schedule.php > /dev/null 2>&1 &' www-data
+    sleep 4;su -c 'php ${_camdir}/schedule.php > /dev/null 2>&1 &' www-data
 else
-    sleep 4;su -s '/bin/bash' -c 'php /var/www$rpicamdir/schedule.php > /dev/null 2>&1 &' www-data
+    sleep 4;su -s '/bin/bash' -c 'php ${_camdir}/schedule.php > /dev/null 2>&1 &' www-data
 fi
 ${RC_END}
 EOF
@@ -158,32 +163,75 @@ EOF
     fi
 }
 
+function fn_check_preconditions ()
+{
+    # Check if we need to move files from an old install to a new location.
+    if [ "${rpicamdir}" != "${rpicamdirold}" ]; then
+        fn_debug "camdir has changed: ${rpicamdirold} -> ${rpicamdir}"
+        if [ -e "/var/www${rpicamdirold}/index.php" ]; then
+            if [ ! -e "/var/www/${rpicamdir}/index.php" ]; then
+                fn_debug "camdirold exists. content move indicated."
+                _move_content=true
+            else
+                fn_warn "camdir changed ({$rpicamdirold} -> ${rpicamdir}), " \
+                        "but new directory already appears populated. "\
+                        "Not copying files." 
+            fi
+        fi
+    fi
+
+    fn_check_missing_or -d "${MJPEG_DEV}"
+    fn_check_missiong_or -d "/etc/raspimjpeg"
+    fn_check_missing_or -l "/usr/bin/raspimjpeg"
+}
+
+
+# Check if we need to create a named fifo, and make sure that an existing
+# file is actually a fifo.
+#
+# \param   filename    filename only of file to test/create.
+#
+function fn_check_fifo()
+{
+    local fifoname="${_camdir}/${1}"; shift
+
+    if [ -e "${fifoname}" -a ! -p "${fifoname}" ]; then
+        fn_displace_to_bak "${fifoname}" "Existing file is not a fifo"
+    fi
+    if [ ! -e "${fifoname}" ]; then
+        sudo mknod "${fifoname}" p
+    fi
+    sudo chmod ugo=rw "${fifoname}"
+}
+
 
 ##############################################################################
 ### Main
 ##############################################################################
 
+# Check any conditions that the user needs to resovle before we can proceed.
+fn_check_preconditions
 
 # Stop the service if it's currently running.
 fn_info "Stopping any running rpicam instance."
 fn_stop
 
-sudo mkdir -p /var/www$rpicamdir/media
+_camdir="/var/www${rpicamdir}"
+
+sudo mkdir -p "${_camdir}/media"
 
 # Move old material if changing from a different install folder
-if [ ! "$rpicamdir" == "$rpicamdirold" ]; then
+if [ "${_move_content}" ]; then
     fn_info "Moving old files to new install folder."
-
-    if [ -e /var/www$rpicamdirold/index.php ]; then
-        sudo mv /var/www$rpicamdirold/* /var/www$rpicamdir
-    fi
+    sudo mv ${_camdir}old/* "${_camdir}"
 fi
 
-sudo cp -r www/* /var/www$rpicamdir/
-if [ -e /var/www$rpicamdir/index.html ]; then
-    sudo rm /var/www$rpicamdir/index.html
-fi
+# Copy over the web content.
+sudo cp -r www/* "${_camdir}"/
+# Make sure there isn't an 'index.html' file blocking index.php
+fn_displace_to_bak "${_camdir}/index.html" "RPiCam uses index.php".
 
+# Run the relevant webserver config.
 if [ "$webserver" == "apache" ]; then
     fn_apache
 elif [ "$webserver" == "nginx" ]; then
@@ -195,53 +243,46 @@ fi
 # Make sure user www-data has bash shell
 sudo sed -i "s/^www-data:x.*/www-data:x:33:33:www-data:\/var\/www:\/bin\/bash/g" /etc/passwd
 
-if [ ! -e /var/www$rpicamdir/FIFO ]; then
-    sudo mknod /var/www$rpicamdir/FIFO p
-fi
-sudo chmod ugo=rw /var/www$rpicamdir/FIFO
+# Make sure the FIFO exists
+fn_check_fifo "FIFO"
+fn_check_fifo "FIFO1"
+fn_check_fifo "FIFO11"
 
-if [ ! -e /var/www$rpicamdir/FIFO11 ]; then
-    sudo mknod /var/www$rpicamdir/FIFO11 p
-fi
-sudo chmod ugo=rw /var/www$rpicamdir/FIFO11
+sudo chmod u=rwx,go=rx "${_camdir}/raspizip.sh"
 
-if [ ! -e /var/www$rpicamdir/FIFO1 ]; then
-    sudo mknod /var/www$rpicamdir/FIFO1 p
-fi
-
-sudo chmod ugo=rw /var/www$rpicamdir/FIFO1
-sudo chmod u=rwx,go=rx /var/www$rpicamdir/raspizip.sh
-
-if [ ! -d /dev/shm/mjpeg ]; then
-    mkdir /dev/shm/mjpeg
+if [ ! -d ${MJPEG_DEV} ]; then
+    mkdir "${MJPEG_DEV}"
 fi
 
 if [ "$jpglink" == "yes" ]; then
-    if [ ! -e /var/www$rpicamdir/cam.jpg ]; then
-       sudo ln -sf /dev/shm/mjpeg/cam.jpg /var/www$rpicamdir/cam.jpg
+    if [ ! -e ${_camdir}/cam.jpg ]; then
+       sudo ln -sf ${MJPEG_DEV}/cam.jpg ${_camdir}/cam.jpg
     fi
 fi
 
-if [ -e /var/www$rpicamdir/status_mjpeg.txt ]; then
-    sudo rm /var/www$rpicamdir/status_mjpeg.txt
+sudo rm -f "${_camdir}/status_mjpeg.txt"
+if [ ! -e "${MJPEG_STATUS}" ]; then
+    echo -n 'halted' >"${MJPEG_STATUS}"
 fi
-if [ ! -e /dev/shm/mjpeg/status_mjpeg.txt ]; then
-    echo -n 'halted' > /dev/shm/mjpeg/status_mjpeg.txt
-fi
-sudo chown www-data:www-data /dev/shm/mjpeg/status_mjpeg.txt
-sudo ln -sf /dev/shm/mjpeg/status_mjpeg.txt /var/www$rpicamdir/status_mjpeg.txt
 
-sudo chown -R www-data:www-data /var/www$rpicamdir
+# Make sure that 'raspimjpeg' isn't running somewhere.
+sleep 1 ; sudo killall raspimjpeg ; sleep 1
+
+sudo chown www-data:www-data "${MJPEG_STATUS}"
+sudo rm -f "${_camdir}/status_mjpeg.txt"
+sudo ln -sf "${MJPEG_STATUS}" ${_camdir}/status_mjpeg.txt
+
+sudo chown -R www-data:www-data "${_camdir}"
 sudo cp etc/sudoers.d/RPI_Cam_Web_Interface /etc/sudoers.d/
 sudo chmod ug=r /etc/sudoers.d/RPI_Cam_Web_Interface
 
-sudo cp -r bin/raspimjpeg /opt/vc/bin/
-sudo chmod u=rwx,go=rx /opt/vc/bin/raspimjpeg
+sudo cp -r bin/raspimjpeg "${RASPIMJPEGDIR}/"
+sudo chmod u=rwx,go=rx "${RASPIMJPEGDIR}/raspimjpeg"
 if [ ! -e /usr/bin/raspimjpeg ]; then
-    sudo ln -s /opt/vc/bin/raspimjpeg /usr/bin/raspimjpeg
+    sudo ln -s "${RASPIMJPEGDIR}/raspimjpeg" /usr/bin/raspimjpeg
 fi
 
-sed -e "s/www/www$rpicamdirEsc/" etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
+sed -e "s#www#www${rpicamdir}#" etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
 if [[ `cat /proc/cmdline |awk -v RS=' ' -F= '/boardrev/ { print $2 }'` == "0x11" ]]; then
     sed -i 's/^camera_num 0/camera_num 1/g' etc/raspimjpeg/raspimjpeg
 fi
@@ -251,13 +292,13 @@ if [ -e /etc/raspimjpeg ]; then
 fi
 sudo cp -r etc/raspimjpeg/raspimjpeg /etc/
 sudo chmod u=rw,go=r /etc/raspimjpeg
-if [ ! -e /var/www$rpicamdir/raspimjpeg ]; then
-    sudo ln -s /etc/raspimjpeg /var/www$rpicamdir/raspimjpeg
+if [ ! -e "${_camdir}/raspimjpeg" ]; then
+    sudo ln -s /etc/raspimjpeg "${_camdir}/raspimjpeg"
 fi
 
 sudo usermod -a -G video www-data
-if [ -e /var/www$rpicamdir/uconfig ]; then
-    sudo chown www-data:www-data /var/www$rpicamdir/uconfig
+if [ -e "${_camdir}/uconfig ]"; then
+    sudo chown www-data:www-data ${_camdir}/uconfig
 fi
 
 fn_info "Configuration motion detector."
@@ -266,18 +307,19 @@ fn_motion
 fn_info "Configuring autostart [(${autostart})]."
 fn_autostart
 
-if [ -e /var/www$rpicamdir/uconfig ]; then
-    sudo chown www-data:www-data /var/www$rpicamdir/uconfig
+if [ -e ${_camdir}/uconfig ]; then
+    sudo chown www-data:www-data ${_camdir}/uconfig
 fi
 
-if [ -e /var/www$rpicamdir/schedule.php ]; then
-    sudo rm /var/www$rpicamdir/schedule.php
+if [ -e "${_camdir}/schedule.php" ]; then
+    sudo rm "${_camdir}/schedule.php"
 fi
 
-sudo sed -e "s/www/www$rpicamdirEsc/g" www/schedule.php > www/schedule.php.1
-sudo mv www/schedule.php.1 /var/www$rpicamdir/schedule.php
-sudo chown www-data:www-data /var/www$rpicamdir/schedule.php
+sudo sed -e "s#www#www${rpicamdir}#g" www/schedule.php > www/schedule.php.1
+sudo mv www/schedule.php.1 "${_camdir}/schedule.php"
+sudo chown www-data:www-data "${_camdir}/schedule.php"
 
 if [ $# -eq 0 ] || [ "$1" != "q" ]; then
     fn_start
 fi
+
